3816fd733844a64c624cca6ec22dfe3e97269830
Kevin Kim <kk946@cornell.edu>
2025-12-13 19:34:23 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
 import "./App.css";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import Home from "./pages/Home";
 import League from "./pages/League";
 
 function App() {
   return (
     <BrowserRouter basename="/rosters">
+      <RedirectToLeague />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/league" element={<League />} />
         <Route path="/league/:sport" element={<League />} />
       </Routes>
     </BrowserRouter>
   );
+}
+
+function RedirectToLeague() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/league/nba");
+  }, [navigate]);
+
+  return null;
 }
 
 export default App;
