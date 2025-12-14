f086fe7411d9f8316504195ef7a761f797aa486b
Kevin Kim <kk946@cornell.edu>
2025-12-14 15:24:10 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,25 @@ import { Routes, Route, BrowserRouter, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import Home from "./pages/Home";
 import League from "./pages/League";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 function App() {
   return (
-    <BrowserRouter basename="/rosters">
-      <RedirectToLeague />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/league" element={<League />} />
-        <Route path="/league/:sport" element={<League />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <BrowserRouter basename="/rosters">
+        <RedirectToLeague />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/league" element={<League />} />
+          <Route path="/league/:sport" element={<League />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
@@ -21,7 +29,7 @@ function RedirectToLeague() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    navigate("/league/nba");
+    navigate("/league/nba", { replace: true });
   }, [navigate]);
 
   return null;
